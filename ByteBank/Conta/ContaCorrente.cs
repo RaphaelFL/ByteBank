517a85628dfbda8cda57ab55ed27ae31fcf597cf
Raphael <52725627+RaphaelFL@users.noreply.github.com>
2023-09-06 23:55:05 -0300

    Initial commit

---
+++ ByteBank/Conta/ContaCorrente.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Titular;
+
+namespace ByteBank.Conta
+{
+    public class ContaCorrente
+    {
+        public static int TotalCriadas{ get; private set; }
+        private int numero_Agencia;
+        public int Numero_Agencia 
+        {
+            get
+            {
+                return this.numero_Agencia;
+            }
+            set 
+            {
+                if (value > 0) 
+                {
+                    this.numero_Agencia = value;
+                }
+            }
+        }
+        public string conta { get; set; }
+        private double saldo = 100;
+        public Cliente titular { get; set; }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor <= saldo)
+            {
+                saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                Sacar(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+        }
+        
+        public void DefinirSaldo(double valor) 
+        {
+            if (valor < 0) 
+            {
+                return;
+            }
+            else 
+            {
+                this.saldo = valor;
+            }
+        }
+        public double ObterSaldo() 
+        {
+            return this.saldo;
+        }
+
+        public ContaCorrente(int numero_Agencia, string conta)
+        {
+            this.Numero_Agencia = numero_Agencia;
+            this.conta = conta;
+            TotalCriadas++;
+        }
+        public ContaCorrente()
+        {
+            TotalCriadas++;
+        }
+    }
+
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,44 @@
+﻿using ByteBank.Conta;
+using ByteBank.Titular;
+
+Cliente clienteAndre = new Cliente();
+clienteAndre.nome = "Andre solva";
+clienteAndre.cpf = "123456789";
+clienteAndre.profissao = "analisador";
+ContaCorrente contaDoAndre = new ContaCorrente();
+contaDoAndre.titular = clienteAndre;
+contaDoAndre.Numero_Agencia = 12;
+contaDoAndre.conta = "1010-x";
+contaDoAndre.DefinirSaldo(100);
+
+Console.WriteLine("Saldo da conta do André = " + contaDoAndre.ObterSaldo());
+
+Console.WriteLine("Saldo da conta do André = " + contaDoAndre.titular.nome);
+
+
+contaDoAndre.Depositar(100);
+
+Console.WriteLine("Saldo da conta do André pôs-deposito = " + contaDoAndre.ObterSaldo());
+
+contaDoAndre.Sacar(300);
+
+if (contaDoAndre.Sacar(300) == true)
+{
+    Console.WriteLine("Saldo da conta do André pôs-deposito = " + contaDoAndre.ObterSaldo());
+}
+else
+{
+    Console.WriteLine("Saldo da conta inferior ao pedido.");
+}
+
+ContaCorrente contaDaMaria = new ContaCorrente(17, "1011-5");
+contaDaMaria.titular = new Cliente();
+contaDaMaria.titular.nome = "Maria Souza";
+contaDaMaria.titular.cpf = "987654321";
+contaDaMaria.titular.profissao = "analisador";
+contaDaMaria.DefinirSaldo (100);
+
+contaDoAndre.transferir(50, contaDaMaria);
+Console.WriteLine("Saldo da conta do André pôs-deposito = " + contaDoAndre.ObterSaldo());
+Console.WriteLine("Saldo da conta do André pôs-deposito = " + contaDaMaria.ObterSaldo());
+Console.WriteLine(ContaCorrente.TotalCriadas);
